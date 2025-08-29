c4fe79f41d0da9add95e81c346d5433257d929da
LeonardoESousa <leonardo.sousa137@gmail.com>
2025-08-29 16:11:20 +0200

    entry point

--- spec2epsilon/__main__.py
+++ spec2epsilon/__main__.py
@@ -4,15 +4,10 @@ import subprocess
 def main():
     path = os.path.join(os.path.dirname(__file__),"susc.ipynb")
     path = r"{} ".format(path)
-    working_dir = os.getcwd()+'/'
-    with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
-        f.write(working_dir+'\n')
     try:
         option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
-        option += ''' --VoilaConfiguration.file_whitelist="['figs/favicon.ico']"'''
         subprocess.check_output(['voila',path,option])
     except subprocess.CalledProcessError:
         option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
-        option += ''' --VoilaConfiguration.file_whitelist="['figs/favicon.ico']"'''
         command = "voila "+path+option
         subprocess.run(command, shell=True)
